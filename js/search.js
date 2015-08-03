cf58bbf3518202db2c870790e7a3a6de6955c322
Денис Зуев <qalter005@mail.ru>
2015-08-03 14:42:36 +0300

    fix search

--- js/search.js
+++ js/search.js
@@ -1,5 +1,10 @@
 var searchButton = document.getElementById('search'), overlay = document.getElementById('search-overlay'),
-    searchContainer = document.getElementById('search-container'), check = true;
-searchButton.onclick = function() {hideTr(overlay)};
-overlay.onclick = function() {hideTr(this)};
-searchContainer.onclick = function() {check = false};
+    searchContainer = document.getElementById('search-container');
+searchButton.onclick = function() {
+    hideTr(overlay);
+    hideTr(searchContainer);
+};
+overlay.onclick = function() {
+    hideTr(this);
+    hideTr(searchContainer);
+};
